c9d02d43482df04efc7d1a5e6e3a0905164cf779
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-15 23:01:57 +0000

    Difference solution without modifying original sentence

--- typerwriter.js
+++ typerwriter.js
@@ -1,10 +1,13 @@
 const sentence = 'hello there from lighthouse labs';
-const sentenceBreak = sentence + '\n'; //Adds new line to the end of the sentence
+// const sentenceBreak = sentence + '\n'; Adds new line to the end of the sentence
 
 let time = 0
-for(const letter of sentenceBreak){
+for(const letter of sentence){
   setTimeout(()=>{
     process.stdout.write(letter)
   }, time)
   time+=50;
 }
+setTimeout(()=>{
+  process.stdout.write('\n')
+}, time)
